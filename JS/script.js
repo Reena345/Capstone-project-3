c79e22c13e1d336cca3f362a464148f6446ba6d6
Your Name <you@example.com>
2024-09-13 06:37:57 +0100

    new change

--- JS/script.js
+++ JS/script.js
@@ -22,7 +22,7 @@
 // Product();
 // 
 
-
+// 
 let API_url = "https://fakestoreapi.com/products";
 
 function Product() {
@@ -33,16 +33,17 @@ function Product() {
     .then((products) => {
       let postProduct =products;
 
-      console.log(postProduct);
+      let newType =document.createElement("div");
+      newType.innerHTML=`
+      <h3>${postProduct.title}</h3>
+      <img src="${postProduct.image}" alt="${postProduct.title}>
+      <p>Price: $${postProduct.price}</p>
+      <p>${postProduct.description}</p>
       
-    //   products.map((post) => {
-        // let newProduct = document.createElement("div");
-
-        // (newProduct.innerHTML = post.id), postProduct.appendChild(newProduct);
-
-        // (newProduct.innerHTML = post.price), postProduct.appendChild(newProduct);
-    //   });
+      `
+      document.body.appendChild(newType)
     });
 }
 
 Product();
+
